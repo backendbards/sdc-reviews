7caff95fe14c58dc2da046c8bd59f6ba1225f05e
Jason Ullrich <jasonullrich@gmail.com>
2024-06-26 16:46:58 -0700

    ETL

--- data/convert.js
+++ data/convert.js
@@ -1,16 +1,16 @@
 import * as fs from 'fs'
+import { rm } from 'fs/promises'
 import * as readline from 'readline'
+import * as path from 'path'
 import csv from 'csv-parser'
 
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-import { drizzle } from "drizzle-orm/node-postgres";
-
-import { reviews } from '../db/schema.js'
+import { drizzle } from "drizzle-orm/node-postgres"
+import { sql } from 'drizzle-orm'
 
 import pg from "pg";
 const { Client } = pg;
 
-const convert = async () => {
+const etl = async () => {
   const client = new Client({
     host: "127.0.0.1",
     port: 5432,
@@ -21,28 +21,69 @@ const convert = async () => {
   await client.connect();
   const db = drizzle(client);
 
-  const results = []
+  console.log('starting')
 
-  const fsStream = fs.createReadStream('./reviews.csv')
-  const csvStream = csv()
+  await db.execute(sql`truncate characteristic_reviews cascade`)
+  console.log('characteristic_reviews emptied')
+  await db.execute(sql`truncate reviews_photos cascade`)
+  console.log('reviews_photos emptied')
+  await db.execute(sql`truncate characteristics cascade`)
+  console.log('characteristics emptied')
+  await db.execute(sql`truncate reviews cascade`)
+  console.log('reviews emptied')
 
-  fsStream
-    .pipe(csvStream)
-    .on('data', async (data) => {
-      if (results.length >= 5) {
-        fsStream.unpipe(csvStream)
-        csvStream.end()
-        fsStream.destroy()
-      } else {
-        data.date = new Date(Number(data.date))
-        results.push(data)
-        console.log(data)
-        await db.insert(reviews).values(data)
-      }
-    })
-    .on('end', () => console.log('done'))
+  await rewriteReviews()
+  await db.execute(sql`copy reviews from '/tmp/sdcdata/reviews_adj.csv' delimiter ',' csv header`)
+  console.log('reviews copied')
 
-    return
+  await db.execute(sql`copy reviews_photos from '/tmp/sdcdata/reviews_photos.csv' delimiter ',' csv header`)
+  console.log('reviews_photos copied')
+
+  await db.execute(sql`copy characteristics from '/tmp/sdcdata/characteristics.csv' delimiter ',' csv header`)
+  console.log('characteristics copied')
+
+  await db.execute(sql`copy characteristic_reviews from '/tmp/sdcdata/characteristic_reviews.csv' delimiter ',' csv header`)
+  console.log('characteristics_reviews copied')
+
+  console.log('all done')
+  process.exit()
+  return true
 }
 
-convert()
+const rewriteReviews = async () => {
+  const file = '/tmp/sdcdata/reviews.csv'
+  const outFile = '/tmp/sdcdata/reviews_adj.csv'
+
+  try {
+    await rm(outFile)
+  } catch (err) {
+    console.log('no existing file')
+  }
+
+  const fsStream = fs.createReadStream(file)
+  const csvStream = csv()
+
+  const headers = ['id', 'product_id', 'rating', 'date', 'summary', 'body', 'recommend', 'reported', 'reviewer_name', 'reviewer_email', 'response','helpfulness']
+
+  const writeStream = fs.createWriteStream(outFile)
+  writeStream.write(headers.join(',') + '\n')
+
+  return new Promise((resolve, reject) => {
+    console.log('reading')
+    fsStream
+    .pipe(csvStream)
+    .on('data', async (data) => {
+      fsStream.pause()
+      if (data.response === 'null') data.response = null
+      data.date = new Date(Number(data.date)).toISOString()
+      writeStream.write(Object.values(data).join(',') + '\n')
+      fsStream.resume()
+    })
+    .on('end', async () => {
+      console.log('done reading')
+      resolve()
+      })
+  })
+}
+
+etl()
--- data/read.js
+++ data/read.js
@@ -1,12 +1,14 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-import { drizzle } from "drizzle-orm/node-postgres";
+import 'dotenv/config.js'
 
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { sql, eq } from 'drizzle-orm'
 import * as schema from '../db/schema.js'
-
-import pg from "pg";
-const { Client } = pg;
+import pg from 'pg'
+const { Client } = pg
+const { reviews, reviewsPhotos } = schema
 
 const read = async () => {
+
   const client = new Client({
     host: "127.0.0.1",
     port: 5432,
@@ -17,10 +19,31 @@ const read = async () => {
   await client.connect();
   const db = drizzle(client, { schema });
 
-  const reviews = await db.query.reviews.findMany()
-  console.log(reviews)
-  console.log(typeof reviews[0].date)
-  return
+  const result = await db.select({
+    id: reviews.id,
+    product_id: reviews.product_id,
+    rating: reviews.rating,
+    date: reviews.date,
+    summary: reviews.summary,
+    body: reviews.body,
+    recommend: reviews.recommend,
+    reported: reviews.reported,
+    reviewer_name: reviews.reviewer_name,
+    reviewer_email: reviews.reviewer_email,
+    response: reviews.response,
+    helpfulness: reviews.helpfulness,
+    photos: sql`coalesce(array_agg(json_build_object('id', reviews_photos.id, 'url', url)) filter (where reviews_photos.id is not null), '{}')`
+  })
+  .from(reviews)
+  .leftJoin(reviewsPhotos, eq(reviews.id, reviewsPhotos.review_id))
+  .groupBy(reviews.id)
+  .limit(5)
+
+  // console.log(JSON.stringify(result, null, 2))
+  console.log(result)
+
+  process.exit()
+  return true
 }
 
 read()
