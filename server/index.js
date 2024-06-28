17c39f714b9f0f3d4e48f9eb84a2bef86e543124
Jason Ullrich <jasonullrich@gmail.com>
2024-06-27 19:10:00 -0700

    Add static server to test frontend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import router from './routes.js'
+import path from 'path'
+const __dirname = import.meta.url.split('/').slice(2,-1).join('/')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, '../../sdc-frontend/dist')));
 app.use(express.json())
 
 app.use('/', router)
