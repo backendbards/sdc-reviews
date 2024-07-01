b2e58be6c7683c9a3c1a7fa362e5a3ce3ee0f773
Jason Ullrich <jasonullrich@gmail.com>
2024-07-01 15:37:35 -0700

    API

--- test/api.test.js
+++ test/api.test.js
@@ -1,10 +1,16 @@
 import http from 'k6/http'
+import { sleep } from 'k6'
 
 export const options = {
-  vus: 300,
-  duration: '10s'
+  vus: 6000,
+  duration: '30s'
 }
 
 export default () => {
-  http.get('http://localhost:3000/reviews?product_id=2')
+
+  const randomID = Math.floor(Math.random() * 5000000) + 1
+
+  http.get(`http://localhost:3000/reviews?product_id=${randomID}`, {timeout: '120s', tags: { name: 'base'}})
+  // http.get(`http://localhost:3000/reviews/meta?product_id=${randomID}`, {timeout: '120s', tags: { name: 'meta'}})
+  sleep(1)
 }
